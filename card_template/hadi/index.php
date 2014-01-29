e0be5b57a38e39bec5884d3c7557b29893b39428
Jushadi Arman Saz <czar.hadi@gmail.com>
2014-01-29 21:33:04 +0800

    Update index.php

--- card_template/hadi/index.php
+++ card_template/hadi/index.php
@@ -29,7 +29,7 @@ $html_str .= '#garis{position:absolute; left: 10px;top: 51px;width:305px;height:
 $html_str .= '#rules_div{-moz-transform:rotateZ(90deg);z-index:4;position: absolute;left: 120px;top:30px;width:165px;height: 142px;text-align: justify;}'."\n";
 $html_str .= '#address_div{-moz-transform:rotateZ(90deg);z-index:4;position: absolute;left: -77px;top: 95px;width:200px;height: 30px;font-size:7px;color: #000000;font-weight: bold;}'."\n";
 $html_str .= '#logo_div{-moz-transform:rotateZ(90deg);z-index:5;position: absolute;left: 275px;top: 5px;}'."\n";
-$html_str .= '#photo_blank_div{-moz-transform:rotateZ(90deg);z-index:5;position: absolute;left: 115px;top:50px;font-size: 7px;text-align: center;border:#cccccc solid 1px;width:'.(($sysconf['membercard_print_settings']['card_photo_width']-0.2)*37.795275591).'px; height:'.(($sysconf['membercard_print_settings']['card_photo_height']-0.2)*37.795275591).'px;}'."\n";
+$html_str .= '#photo_blank_div{-moz-transform:rotateZ(90deg);z-index:5;position: absolute;left: 115px;top:50px;font-size: 7px;text-align: center;border:#cccccc solid 1px;width:'.(($sysconf['membercard_print_settings']['card_photo_width']-0.2)*37.795275591).'px; height:'.(($sysconf['membercard_print_settings']['card_photo_height']-0.2)*37.795275591).'px;}'."\n";                                                                       //Jushadi  A r m an   S az
 $html_str .= '#photo_div{-moz-transform:rotateZ(90deg);z-index:6;position: absolute;left: 115px;top:50px;width:'.(($sysconf['membercard_print_settings']['card_photo_width']+0.2)*37.795275591).'px; height:'.($sysconf['membercard_print_settings']['card_photo_height']*37.795275591).'px;}'."\n";
 $html_str .= '.bio_div{-moz-transform:rotateZ(90deg);z-index:7;position: absolute;left: -50px;top:55px;height: 100px;margin: 0px;text-align: center;width: 50%;}'."\n";
 $html_str .= '.bio_label{z-index:10;float: left; width: 100%;margin-bottom:0px;margin-top: 0px;margin-left:0px;font-weight: bold;}'."\n";
@@ -42,7 +42,7 @@ $html_str .= '.pejabat{z-index:15;top: 0px;font-size: 8px;margin: 0px;}'."\n";
 $html_str .= '.gambar_ttd_div{z-index:16;position: absolute;left: 0px;top: 20px;width:107px;height: 25px;}'."\n";
 $html_str .= '.gambar_stempel_div{z-index:17;position: absolute;left:-10px;top: 15px;width: 40px;height: 40px;}'."\n";
 $html_str .= '.mtype{z-index:199;position: absolute;border-radius: 4px 4px 4px 4px;left: 0px;top: 0px;width:83px;height: 12px;font-size: 9px;text-align: center;font-weight: bold;}'."\n";
-$html_str .= '.mtype_div{-moz-transform:rotateZ(90deg);z-index:199;border: medium ridge royalblue;position: absolute;border-radius: 4px 4px 4px 4px;left: 50px;top: 95px;width:83px;height: 12px;font-size: 9px;text-align: center;font-weight: bold;}'."\n";
+$html_str .= '.mtype_div{-moz-transform:rotateZ(90deg);z-index:199;border: medium ridge royalblue;position: absolute;border-radius: 4px 4px 4px 4px;left: 50px;top: 95px;width:83px;height: 12px;font-size: 9px;text-align: center;font-weight: bold;}'."\n"; // when I notice my name deleted again :)
 $html_str .= '.exp_div{z-index:18;position: absolute;left: 90px;top: 185px;width:135px;height: 12px;font-size: 9px;text-align: left;}'."\n";
 $html_str .= '.barcode_div{z-index:19;position: absolute;left: 70px;top: 10px;width:160px;}'."\n";
 $html_str .= '</style>'."\n";
@@ -55,7 +55,7 @@ $html_str .= '<table cellspacing="0" cellpadding="20" border="1">'."\n";
         foreach ($card_rows as $card) {
             $html_str .= '<td valign="top">';
 $html_str .= '<div id="kontainer_div">';
-$html_str .= '<div><img width="'.($sysconf['membercard_print_settings']['card_box_width']*37.795275591).'px" height="'.($sysconf['membercard_print_settings']['card_box_height']*37.795275591).'px" src="'.SENAYAN_WEB_ROOT_DIR.IMAGES_DIR.'/member_card/Frontcard_'.$card['member_type_name'].'" style="border-radius: 8px; -moz-border-radius: 8px;-khtml-border-radius: 8px;-webkit-border-radius: 8px;"></img></div>';
+$html_str .= '<div><img width="'.($sysconf['membercard_print_settings']['card_box_width']*37.795275591).'px" height="'.($sysconf['membercard_print_settings']['card_box_height']*37.795275591).'px" src="'.SENAYAN_WEB_ROOT_DIR.IMAGES_DIR.'/member_card/Frontcard_'.$card['member_type_name'].'" style="border-radius: 8px; -moz-border-radius: 8px;-khtml-border-radius: 8px;-webkit-border-radius: 8px;"></img></div>'; // Jus  Hadi  Arm An Saz
 $html_str .= '<div id="logo_div"><img height="45px" width="45px" src="'.SENAYAN_WEB_ROOT_DIR.IMAGES_DIR.'/member_card/Logo_'.$card['member_type_name'].'"></img></div>';
 $html_str .= '<div id="header1_div"><img style="width: 100%; height:100%; border-top-left-radius: 8px;border-top-right-radius: 8px;" src="/x3/images/member_card/1blue.png">';
 $html_str .= '<div id="header1">';
@@ -77,7 +77,7 @@ $html_str .= ''.( $sysconf['membercard_print_settings']['card_show_mail_address_
 
 $html_str .= '</div>';
 $html_str .= '<div id="photo_blank_div"><br /><br />Foto Ukuran:<br />'.$sysconf['membercard_print_settings']['card_photo_width'].' X '.$sysconf['membercard_print_settings']['card_photo_height'].' cm</div>';
-$html_str .= '<div id="photo_div"><div align="center"><img width="'.($sysconf['membercard_print_settings']['card_photo_width']*37.795275591+2).'px" height="'.($sysconf['membercard_print_settings']['card_photo_height']*37.795275591+2).'px" style="border-radius: 4px; -moz-border-radius: 4px;-khtml-border-radius: 4px;-webkit-border-radius: 4px;" src="'.SENAYAN_WEB_ROOT_DIR.IMAGES_DIR.'/persons/'.$card['member_image'].'"/></img></div></div>';
+$html_str .= '<div id="photo_div"><div align="center"><img width="'.($sysconf['membercard_print_settings']['card_photo_width']*37.795275591+2).'px" height="'.($sysconf['membercard_print_settings']['card_photo_height']*37.795275591+2).'px" style="border-radius: 4px; -moz-border-radius: 4px;-khtml-border-radius: 4px;-webkit-border-radius: 4px;" src="'.SENAYAN_WEB_ROOT_DIR.IMAGES_DIR.'/persons/'.$card['member_image'].'"/></img></div></div>';//Jushadi Arman  Saz
 $html_str .= ''.( $sysconf['membercard_print_settings']['card_show_type_label']?'':'<!--').'<div class="mtype_div"><img style="width: 100%; height:100%; border-radius: 1px;" src="/x3/images/member_card/1white.png"><div class="mtype">'.$card['member_type_name'].'</div></div>'.( $sysconf['membercard_print_settings']['card_show_type_label']?'':'-->').''; //Jushadi
 $html_str .= ''.( $sysconf['membercard_print_settings']['card_show_expired_label']?'':'<!--').'<div class="exp_div">'.__('Expiry Date').' : '.$card['expire_date'].'</div>'.( $sysconf['membercard_print_settings']['card_show_expired_label']?'':'-->').'';
 $html_str .= ''.( $sysconf['membercard_print_settings']['card_show_barcode_label']?'':'<!--').'<div class="barcode_div">';
@@ -97,12 +97,12 @@ $html_str .= '<h2>'.$sysconf['membercard_print_settings']['card_front_header2_te
 //$html_str .= '<h4>'.$sysconf['membercard_print_settings']['card_back_header2_text'].'</h4></div>'.( $sysconf['membercard_print_settings']['card_show_back_header']?'':'-->').'';
 $html_str .= ''.( $sysconf['membercard_print_settings']['card_show_back_hlines']?'':'<!--').'<div id="garis"><hr></div>'.( $sysconf['membercard_print_settings']['card_show_back_hlines']?'':'-->').'';
 $html_str .= '<div id="rules_div"><ul>'.$sysconf['membercard_print_settings']['card_rules'].'</ul></div>';
-$html_str .= '<div id="address_div">'.$sysconf['membercard_print_settings']['card_address'].'</div>';
+$html_str .= '<div id="address_div">'.$sysconf['membercard_print_settings']['card_address'].'</div>'; // Jushadi  Arman Saz
 $html_str .= ''.( $sysconf['membercard_print_settings']['card_show_back_stamp']?'':'<!--').'<div class="stempel_div">';
 $html_str .= '<div class="gambar_stempel_div"><img class="" height="35px" width="35px" src="'.SENAYAN_WEB_ROOT_DIR.IMAGES_DIR.'/member_card/Stamp_'.$card['member_type_name'].'"></img></div>';
 $html_str .= '<div class="gambar_ttd_div"><img class="" height="30px" width="100px" src="'.SENAYAN_WEB_ROOT_DIR.IMAGES_DIR.'/member_card/Signature_'.$card['member_type_name'].'"></img></div>';
 $html_str .= '<p class="stempel lokasi">'.$sysconf['membercard_print_settings']['card_city'].', '.$card['register_date'].'</p><p class="stempel jabatan">'.$sysconf['membercard_print_settings']['card_title'].'</p><br><br>';
-$html_str .= '<p class="stempel pejabat">'.$sysconf['membercard_print_settings']['card_officials'].'</br>'.$sysconf['membercard_print_settings']['card_officials_id'].'</p></div></div>'.( $sysconf['membercard_print_settings']['card_show_back_stamp']?'':'-->').'';
+$html_str .= '<p class="stempel pejabat">'.$sysconf['membercard_print_settings']['card_officials'].'</br>'.$sysconf['membercard_print_settings']['card_officials_id'].'</p></div></div>'.( $sysconf['membercard_print_settings']['card_show_back_stamp']?'':'-->').''; // Jushadi Arman  Saz
 $html_str .= '</td>';
         }
         $html_str .= '<tr>'."\n";
