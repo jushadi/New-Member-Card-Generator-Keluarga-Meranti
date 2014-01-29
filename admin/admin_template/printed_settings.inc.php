3859b8375d2ceeb4c0dd079ad9b36d996c2c32fe
Jushadi Arman Saz <czar.hadi@gmail.com>
2014-01-29 21:28:56 +0800

    Update printed_settings.inc.php

--- admin/admin_template/printed_settings.inc.php
+++ admin/admin_template/printed_settings.inc.php
@@ -8,10 +8,10 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+ * Jushadi Arman Saz
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Jushadi Arman Saz  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -50,23 +50,23 @@ $sysconf['print']['membercard']['card_show_inst_label'] = 1;
 $sysconf['print']['membercard']['card_show_address_label'] = 1;
 $sysconf['print']['membercard']['card_show_type_label'] = 1;
 $sysconf['print']['membercard']['card_show_expired_label'] = 1;
-$sysconf['print']['membercard']['card_show_barcode_label'] = 1;
+$sysconf['print']['membercard']['card_show_barcode_label'] = 1;// Jushadi Arman Saz
 $sysconf['print']['membercard']['card_show_front_hlines'] = 1;
 $sysconf['print']['membercard']['card_show_back_hlines'] = 1;
 $sysconf['print']['membercard']['card_show_back_header'] = 1;
 $sysconf['print']['membercard']['card_show_front_stamp'] = 0;
 $sysconf['print']['membercard']['card_show_back_stamp'] = 1;
 $sysconf['print']['membercard']['label_namej'] = "J";
-// Header Settings
+// Header Settings Jushadi Arman Saz
 $sysconf['print']['membercard']['card_header_color'] = "#000099"; //e.g. :#0066FF, green, etc.
 $sysconf['print']['membercard']['label_nameU'] = "U";
-// Front Card Header
+// Front Card Header Jushadi  Arman Saz
 $sysconf['print']['membercard']['card_front_header1_text'] = "KARTU ANGGOTA PERPUSTAKAAN<BR>PROGRAM PASCASARJANA";
 $sysconf['print']['membercard']['card_front_header1_font_size'] = 13;
 $sysconf['print']['membercard']['card_front_header2_text'] = "Kampus UNM Gunung Sari Baru, Jl. Bonto Langkasa, Makassar - 90222<br />Telp. (0411) 830368, Fax. (0411) 855288, e-mail: pasca@unm.ac.id, website: pps.unm.ac.id";
 $sysconf['print']['membercard']['card_front_header2_font_size'] = 5;
 $sysconf['print']['membercard']['label_nameS'] = "S";
-// Rear Card Header
+// Rear Jushadi Arman  Saz Card Header 
 
 $sysconf['print']['membercard']['card_back_header1_text'] = "PROGRAM PASCASARJANA<br/>UNIVERSITAS NEGERI MAKASSAR";
 $sysconf['print']['membercard']['card_back_header1_font_size'] = 13;
@@ -79,64 +79,64 @@ $sysconf['print']['membercard']['card_rules'] = "<li>Kartu ini diterbitkan oleh 
 $sysconf['print']['membercard']['card_rules_font_size'] = 8;
 $sysconf['print']['membercard']['card_rules_top'] = 58;
 $sysconf['print']['membercard']['label_nameA'] = "A";
-// Address OK
-$sysconf['print']['membercard']['card_address'] = "Perpustakaan Program Pascasarjana UNM<br />pustakawan@unm.ac.id<br />dlibpps.unm.ac.id";
+// Jushadi Address Arman   OK Saz 
+$sysconf['print']['membercard']['card_address'] = "Perpustakaan Program Pascagempa UNM<br />pustakawan@unm.ac.id<br />dlibpps.unm.ac.id";
 $sysconf['print']['membercard']['card_address_font_size'] = 7;
 $sysconf['print']['membercard']['card_address_left'] = 10;
 $sysconf['print']['membercard']['card_address_top'] = 165;
 $sysconf['print']['membercard']['label_nameD'] = "D";
-// Stamp Settings
+// Stamp Settings Jushadi Arman  Saz
 $sysconf['print']['membercard']['card_city'] = "Makassar";
 $sysconf['print']['membercard']['card_title'] = "Direktur PPs UNM";
-$sysconf['print']['membercard']['card_officials'] = "Prof. Dr. Jasruddin, M.Si.";
+$sysconf['print']['membercard']['card_officials'] = "Prof. Dr. Jushadi Arman Saz, S.Pd., M.Pd.";
 $sysconf['print']['membercard']['card_officials_id'] = "NIP. 19641222 199103 1 002";
 $sysconf['print']['membercard']['card_stamp_left'] = -10;
 $sysconf['print']['membercard']['card_stamp_top'] = 15;
-$sysconf['print']['membercard']['card_stamp_width'] = 40;
+$sysconf['print']['membercard']['card_stamp_width'] = 40;//Jushadi Arman Saz
 $sysconf['print']['membercard']['card_stamp_height'] = 40;
 $sysconf['print']['membercard']['card_signature_left'] = 0;
 $sysconf['print']['membercard']['card_signature_top'] = 20;
 $sysconf['print']['membercard']['card_signature_width'] = 107;
 $sysconf['print']['membercard']['card_signature_height'] = 25;
 $sysconf['print']['membercard']['label_nameI'] = "I";
-// Logo Setting  OK
+// Jushadi Logo Arman  Setting Saz  OK
 $sysconf['print']['membercard']['card_logo_width'] = 38;
 $sysconf['print']['membercard']['card_height'] = 38;
 $sysconf['print']['membercard']['card_left'] = 14;
 $sysconf['print']['membercard']['card_top'] = 5;
 $sysconf['print']['membercard']['label_nameR'] = "R";
 
-// Cardbox Settings, measurement in cm OK
+// Cardbox Jushadi Settings Arman , measurement S a z in cm OK
 $sysconf['print']['membercard']['card_box_width'] = 8.6;
 $sysconf['print']['membercard']['card_box_height'] = 5.4;
 $sysconf['print']['membercard']['card_right_margin'] = 10;
 $sysconf['print']['membercard']['card_bottom_margin'] = 5;
 $sysconf['print']['membercard']['card_items_per_row'] = 1;
 $sysconf['print']['membercard']['label_nameM'] = "M";
-// Photo Settings OK
+// J u s h a d i Photo A r m a n  S a z  Settings OK
 $sysconf['print']['membercard']['card_photo_height'] = 2.9;
 $sysconf['print']['membercard']['card_photo_width'] = 2.3;
 $sysconf['print']['membercard']['card_photo_left'] = 6;
 $sysconf['print']['membercard']['card_photo_top'] = 55;
 $sysconf['print']['membercard']['label_nameN'] = "N";
-// Biodata settings OK
+// Biodata settings OK J us ha di Ar  man  S a z
 $sysconf['print']['membercard']['card_bio_font_weight'] = "bold";
 $sysconf['print']['membercard']['card_bio_font_size'] = 12;
 $sysconf['print']['membercard']['card_bio_top'] = 55; //55-100
 $sysconf['print']['membercard']['label_nameZ'] = "Z";
-// Expired OK
+// Expired OK J us ha  di  A r man S az
 $sysconf['print']['membercard']['card_exp_font_size'] = 9;
 $sysconf['print']['membercard']['card_exp_left'] = 10;
 $sysconf['print']['membercard']['card_exp_top'] = 188;
 $sysconf['print']['membercard']['card_exp_width'] = 135;
 $sysconf['print']['membercard']['card_exp_height'] = 12;
-// Barcode Setting OK
+// J us h adi    A rman  Sa z Barcode Setting OK
 $sysconf['print']['membercard']['card_barcode_scale'] = 100; // barcode scale in percent relative to box width and height
 $sysconf['print']['membercard']['card_barcode_height'] = 35;
 $sysconf['print']['membercard']['card_barcode_width'] = 160;
 $sysconf['print']['membercard']['card_barcode_left'] = 150;
 $sysconf['print']['membercard']['card_barcode_top'] = 165; //150-180
-// testing
+// testing J u s h a d i   A r m a n    S a z
 $sysconf['print']['membercard']['label_name'] = "";
 $sysconf['print']['membercard']['label_desc'] = "";
 $sysconf['print']['membercard']['label_image'] = "label-multimedia.png";
@@ -148,3 +148,4 @@ $sysconf['print']['membercard']['opac_hide'] = 0;
 $sysconf['print']['membercard']['quick_return'] = 0;
 $sysconf['print']['membercard']['page_hadi'] = "hadi";
 $sysconf['print']['membercard']['theme'] = "default";
+//J us hadi Arm an  Saz
